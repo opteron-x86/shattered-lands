3a439d6556574b922beda8e35f127669416cfff6
opteron-x86 <opteronx86@protonmail.com>
2025-01-07 14:16:08 +0000

    resources def

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -3,28 +3,9 @@ import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 const schema = a.schema({
   User: a
     .model({
-      // For simplicity: one required field
       username: a.string().required(),
-
-      // The relationship: A user has many kingdoms.
-      // 1) 'Kingdom' is the child model's name.
-      // 2) 'userId' is the **exact** field name in the child model
-      //    that references this user.
-      kingdoms: a.hasMany('Kingdom', 'userId'),
-    })
-    .authorization((rules) => [rules.owner()]),
-
-  Kingdom: a
-    .model({
-      name: a.string().required(),
-      resources: a.json(),
-      buildings: a.json(),
-      troops: a.json(),
-
-      // This is the "childField" from above, i.e. 'userId'.
-      // We'll define it as "belongsTo('User','id')"
-      // meaning it references the User model's 'id' field.
-      userId: a.belongsTo('User', 'id'),
+      createdAt: a.datetime(),
+      updatedAt: a.datetime(),
     })
     .authorization((rules) => [rules.owner()]),
 });
